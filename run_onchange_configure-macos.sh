5ff96ee12a6b6c5029407d7a3d1c653393c4e322
Jona Koudijs <nb06ff@insim.biz>
2025-02-14 16:34:11 +0100

    Auto push changes

--- run_onchange_configure-macos.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# S screenshot save location
-defaults write com.apple.screencapture location ~/Downloads
-
-# Show volume button 
-defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.volume" -bool true
